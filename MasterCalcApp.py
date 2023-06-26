621dc5369fd1042d32431e78ef62b6b0445d00ba
AlaaAliAlrajhy <131582181+AlaaAliAlrajhy@users.noreply.github.com>
2023-06-26 19:34:32 +0200

    Update MasterCalcApp.py

--- MasterCalcApp.py
+++ MasterCalcApp.py
@@ -1,13 +1,17 @@
 import tkinter
 from tkinter import *
 
+# Create the window for the calculator:
 root = Tk()
 root.title("My First Calculator")
 root.geometry("570x700+400+400")
 root.configure(bg="#FFFDD0")
 
+# Initialize the variable to hold the mathematical equation:
+
 equation = ""
 
+# Function to update the equation and display the result:
 def show(value):
     global equation
     equation += value
@@ -17,7 +21,8 @@ def clear():
     global equation
     equation = ""
     label_result.config(text=equation)
-
+    
+# Function to evaluate and calculate the equation:
 def calculate():
     global equation
     try:
@@ -28,14 +33,18 @@ def calculate():
         equation = "Syntax Error"
         label_result.config(text=equation)
     except:
+# Handle any other error that may occur during calculation:
         equation = "Error"
         label_result.config(text=equation)
+        
+# Create a label widget to display the result:
 
 label_result = Label(root, width=25, height=2, text="", font=('Arial', 30,), bg="#F5F5DC")
 label_result.pack()
+# Create the button functions:
 
-Button(root, text="C", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#e61736", command=lambda: clear()).place(x=10, y=100)
-Button(root, text="/", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#17183d", command=lambda: show("/")).place(x=150, y=100)
+Button(root, text="C", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#e61736", command=lambda: clear()).place(x=10, y=100)    #clear the display
+Button(root, text="/", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#17183d", command=lambda: show("/")).place(x=150, y=100) #show the "/"
 Button(root, text="%", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#17183d", command=lambda: show("%")).place(x=290, y=100)
 Button(root, text="*", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#17183d", command=lambda: show("*")).place(x=430, y=100)
 Button(root, text="7", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#17183d", command=lambda: show("7")).place(x=10, y=200)
@@ -48,7 +57,7 @@ Button(root, text="6", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="
 Button(root, text="1", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#17183d", command=lambda: show("1")).place(x=10, y=400)
 Button(root, text="2", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#17183d", command=lambda: show("2")).place(x=150, y=400)
 Button(root, text="3", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#17183d", command=lambda: show("3")).place(x=290, y=400)
-Button(root, text="=", width=5, height=3, font=("arial", 30, "bold"), bd=1, fg="#e61736", bg="#17183d", command=calculate).place(x=430, y=410)
+Button(root, text="=", width=5, height=3, font=("arial", 30, "bold"), bd=1, fg="#e61736", bg="#17183d", command=calculate).place(x=430, y=410)        #show the calculation resault
 Button(root, text="00", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#17183d", command=lambda: show("00")).place(x=10, y=500)
 Button(root, text="0", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#17183d", command=lambda: show("0")).place(x=150, y=500)
 Button(root, text=".", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#17183d", command=lambda: show(".")).place(x=290, y=500)
